3dddb27f994b1225504d0d4894de5eb39210c086
hamza akram <hamzaakram587@gmail.com>
2023-08-24 21:22:19 +0500

    change in leftnav

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            "f9950d46aamsh97df366c6825ffdp1ecc7ejsn8c8e6b31ddf3" ,
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
